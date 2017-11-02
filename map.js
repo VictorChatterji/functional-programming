bb2fa75416325a87e62469dee0c776c0ed7f645e
Victor <chatterji.nani@gmail.com>
2017-11-02 16:18:32 +0530

    chaned the map to see it as higher order function

--- map.js
+++ map.js
@@ -2,7 +2,10 @@
 
     'use strict';
     const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    const myMap = myArray.map(x => x * 2);
+
+    const double = x => x * 2;
+    const myMap = myArray.map(double);
+
     console.log(myMap);
 
 })();
