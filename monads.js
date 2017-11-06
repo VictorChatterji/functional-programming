11647d4ade2ad6a3daf36e42768d1ad18c57df64
Victor <chatterji.nani@gmail.com>
2017-11-06 14:15:49 +0530

    monads partial commit

---
+++ monads.js
@@ -0,0 +1,21 @@
+(function(){
+    'use strict';
+    //Composition: the act of combining the parts and elements to form a whole
+    //The act of breaking the complex problem into simple problems
+    //what is function composition ?
+    //what is object composition ?
+    
+
+    //the math way of seeing the code
+
+    const f = x => x + 1;
+    const g = x => x * 2;
+
+    const h = x => {
+        const afterG = g(x);
+	const afterF = f(afterG);
+	return afterF;
+    }
+
+    console.log(h(2));
+})();
