78849ee01b9e3babb14ed038693cb2cc7c3e7080
Victor <chatterji.nani@gmail.com>
2017-11-15 12:34:53 +0530

    file renamed

--- compose.js
+++ compose.js
@@ -1,10 +1,10 @@
-(function(){
+(function() {
     'use strict';
     //Composition: the act of combining the parts and elements to form a whole
     //The act of breaking the complex problem into simple problems
     //what is function composition ?
     //what is object composition ?
-    
+
 
     //the math way of seeing the code
 
@@ -13,8 +13,8 @@
 
     const h = x => {
         const afterG = g(x);
-	const afterF = f(afterG);
-	return afterF;
+        const afterF = f(afterG);
+        return afterF;
     }
 
     console.log(h(2));
